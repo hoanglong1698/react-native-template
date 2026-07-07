389e597568bbd4a8f9eeb3a8acb86fdf178dc0af
Hoang Long <hoanglong.nguyen.1698@gmail.com>
2026-07-07 17:30:44 +0700

    config nativewind

--- template/App.tsx
+++ template/App.tsx
@@ -1,15 +1,19 @@
+import "./global.css"
 import { AppNavigation } from '@/navigation';
 import { StatusBar, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppNavigation />
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppNavigation />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
--- template/babel.config.js
+++ template/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
     [
       'module-resolver',
@@ -9,6 +9,7 @@ module.exports = {
           '@': './src',
         },
       },
+      'react-native-worklets/plugin',
     ],
   ],
 };
