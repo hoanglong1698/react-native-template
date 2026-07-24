bbb35233df34f96546b2bd674b8344d88f5429df
Hoang Long <hoanglong.nguyen.1698@gmail.com>
2026-07-24 17:17:47 +0700

    add expo module

--- template/android/app/src/main/java/com/projectname/MainActivity.kt
+++ template/android/app/src/main/java/com/projectname/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.projectname
 
+import expo.modules.ReactActivityDelegateWrapper
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -21,7 +22,7 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
 
   override fun onCreate(savedInstanceState: Bundle?) {
     supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
