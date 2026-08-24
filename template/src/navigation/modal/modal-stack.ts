df9839f2c3d7b0efc93db01c54ca4efdbbb41fee
Hoang Long <hoanglong.nguyen.1698@gmail.com>
2026-08-24 16:09:12 +0700

    update animation modalfy

--- template/src/navigation/modal/modal-stack.ts
+++ template/src/navigation/modal/modal-stack.ts
@@ -1,68 +1,39 @@
-import { Easing } from 'react-native';
-import {
-  createModalStack,
-  ModalOptions,
-  ModalStackConfig,
-} from 'react-native-modalfy';
+import { createModalStack, ModalOptions, ModalStackConfig } from 'react-native-modalfy';
 import { AppAlertModal, AppLoadingModal } from '../../components/modals';
 import { ModalName, ModalStackParamsList } from './modal.type';
+
+const MODAL_ANIMATION_TIME = 300;
 
 const modalConfig: ModalStackConfig = {
   [ModalName.AppAlert]: {
     modal: AppAlertModal,
-    position: 'center',
     transitionOptions: animatedValue => ({
       opacity: animatedValue.interpolate({
         inputRange: [0, 1, 2],
         outputRange: [0, 1, 0],
       }),
-      transform: [
-        {
-          scale: animatedValue.interpolate({
-            inputRange: [0, 1, 2],
-            outputRange: [0.92, 1, 0.95],
-          }),
-        },
-      ],
     }),
   },
   [ModalName.AppLoading]: {
     modal: AppLoadingModal,
-    position: 'center',
     backBehavior: 'none',
+    disableFlingGesture: true,
     transitionOptions: animatedValue => ({
       opacity: animatedValue.interpolate({
         inputRange: [0, 1, 2],
         outputRange: [0, 1, 0],
       }),
-      transform: [
-        {
-          scale: animatedValue.interpolate({
-            inputRange: [0, 1, 2],
-            outputRange: [0.9, 1, 0.95],
-          }),
-        },
-      ],
     }),
   },
 };
 
 const defaultOptions: ModalOptions = {
   position: 'center',
-  backdropOpacity: 0.5,
-  backdropColor: 'black',
+  backdropOpacity: 0.6,
   backBehavior: 'pop',
-  animateInConfig: {
-    duration: 300,
-    easing: Easing.out(Easing.ease),
-  },
-  animateOutConfig: {
-    duration: 250,
-    easing: Easing.in(Easing.ease),
-  },
+  animateInConfig: { duration: MODAL_ANIMATION_TIME },
+  animateOutConfig: { duration: MODAL_ANIMATION_TIME },
+  backdropAnimationDuration: MODAL_ANIMATION_TIME,
 };
 
-export const modalStack = createModalStack<ModalStackParamsList>(
-  modalConfig,
-  defaultOptions,
-);
+export const modalStack = createModalStack<ModalStackParamsList>(modalConfig, defaultOptions);
